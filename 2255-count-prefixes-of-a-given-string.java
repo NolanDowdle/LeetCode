2eb4251efab1779ad9086b59b366d59dbf74232c
Nolan Dowdle <njdowdle@gmail.com>
2023-07-17 01:12:40 +0700

    Update 2255-count-prefixes-of-a-given-string.java
    O(n) because we use String.startsWith() method to reduce run-time from O(n^2) to O(n)

--- 2255-count-prefixes-of-a-given-string.java
+++ 2255-count-prefixes-of-a-given-string.java
@@ -1,19 +1,9 @@
 class Solution {
     public int countPrefixes(String[] words, String s) {
         int count = 0;
-        for(int i = 0; i < words.length; i++) {
-            for(int j = 0;j < words[i].length(); j++) {
-                if(j > s.length()-1) {
-                    break;
-                }
-                if(words[i].charAt(j) != s.charAt(j)) {
-                    break;
-                } else {
-                    if(j == words[i].length()-1) {
-                        System.out.println(words[i]);
-                        count += 1;
-                    }
-                }
+        for(String word: words) {
+            if(s.startsWith(word)) {
+                count++;
             }
         }
         return count;
